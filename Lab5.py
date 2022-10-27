a3145380bef595ccf7f5fdeac14e758d83d1d5e4
Brian Huante Lopez <b_huantelopez@u.pacific.edu>
2022-10-26 18:06:38 -0700

    added new code

--- Lab5.py
+++ Lab5.py
@@ -6,23 +6,41 @@
 import sys
 
 
-def fcfs(p, p_lines):
+def fcfs(p, p_dict):
     fcfs_dict = {}
-    for line in p_lines:
-        fcfs_dict[int(line.split()[0])] = [int(line.split()[1]), int(line.split()[2])]
+    counter = 0
     for i in range(p):
-        list_of_keys = [key
-                        for key, list_of_values in fcfs_dict.items()
-                        if i in list_of_values]
+        lowestStartProcess = p_dict.keys()[0]
+        for process, valueArr in p_dict.items():
+            if counter == i:
+                continue
+
+            if valueArr[0] == p_dict[lowestStartProcess][0]:
+                if process < lowestStartProcess:
+                    lowestStartProcess = process
+            elif valueArr[0] < p_dict[lowestStartProcess][0]:
+                lowestStartProcess = process
+
+        fcfs_dict[lowestStartProcess] = p_dict[lowestStartProcess]
+        p_dict.pop(lowestStartProcess)
+        counter += 1
+
+    print('FCFC:\n')
+    print('\t\tPID\tArrival\tStart Time\tEnd Time\tRunning\tWaiting')
 
 
 n = len(sys.argv)
 if n < 3:
     sys.exit("Not enough values in command line.")
 
+processDict = {}
+
 with open('filename') as f:
     lines = [line.rstrip('\n') for line in f]
 numProcesses = int(lines[0])
 lines.pop(0)
 
-fcfs(numProcesses, lines)
+for line in lines:
+    processDict[int(line.split()[0])] = [int(line.split()[1]), int(line.split()[2])]
+
+fcfs(numProcesses, processDict)
